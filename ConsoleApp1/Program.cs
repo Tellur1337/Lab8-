4ce4153d4d4e5936d484d1b083250b715362468e
NIKITA REPIN <nikitka.repin.2017@mail.ru>
2023-11-19 13:46:41 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace ConsoleApp1
+{
+    public class Lab1
+    {
+        static void Main(string[] args)
+        {
+            // #1
+            //int[] inData = new int[] { 1, 4, 7, 3, 6, 8, 2, 0, 9, 5 };
+            //int[] inData2 = new int[] { 1, 4, 7, 3, 6, 8, 2, 0, 9, 5 };
+
+
+            //int[] result = Sorting(inData);
+            //foreach (int i in result) 
+            //    Console.Write(i + " ");
+            //Console.WriteLine();
+
+
+            //Console.WriteLine(DoubleNum(3.45, 2));
+
+            // #2
+
+            //if (Palindrom("1234321"))                               
+            //    Console.WriteLine(true);
+            //else
+            //    Console.WriteLine(false);
+            //if (Palindrom("12334321"))                               
+            //    Console.WriteLine(true);
+            //else
+            //    Console.WriteLine(false);
+            //
+            //// #3
+            //
+            //Console.WriteLine(Factorial(6));
+            //
+            //// #4
+            //
+            //Console.WriteLine(Fibonacci(10));
+            //
+            //// #5
+            //
+            //Console.WriteLine(DoubleNum(2.2, 3.4));                         
+            //Console.WriteLine(DoubleNum(12.432, 3.344));
+            //Console.WriteLine(DoubleNum(24.62, 7.33));
+            //
+            //// #6
+            //
+            //Console.WriteLine(SimpleDigit(11));                          
+            //Console.WriteLine(SimpleDigit(21));
+
+        }
+
+        public static int[] Sorting(int[] massive)
+        {
+            if (massive.Length == 0)
+                throw new Exception("Массив не может быть пустым! ");
+            int temp;
+            for (int i = 0; i < massive.Length; i++)
+            {
+                for (int j = i + 1; j < massive.Length; j++)
+                {
+                    if (massive[i] > massive[j])
+                    {
+                        temp = massive[i];
+                        massive[i] = massive[j];
+                        massive[j] = temp;
+                    }
+                }
+            }
+            return massive;
+        }
+        public static bool Palindrom(string stroka)
+        {
+            for (int i = 0; i < stroka.Length / 2; i++)
+            {
+                if (stroka[i] == stroka[stroka.Length - i - 1])
+                    continue;
+                else
+                    return false;
+            }
+            return true;
+        }
+
+        public static int Factorial(int number)
+        {
+            if (number <= 0)
+                throw new Exception("Число не может быть меньше единицы! ");
+            int count = 1;
+            for (int i = 1; i <= number; i++)
+                count *= i;
+
+            return count;
+        }
+
+        public static int Fibonacci(int num)
+        {
+            if (num < 0)
+                throw new Exception("Число не может быть меньше нуля! ");
+            if (num == 0)
+                return 0;
+            int prev = 0;
+            int next = 1;
+            for (int i = 1; i < num; i++)
+            {
+                int sum = prev + next;
+                prev = next;
+                next = sum;
+            }
+            return next;
+        }
+
+        public static double DoubleNum(double num, double degree) => Math.Round(Math.Pow(num, degree), 3);
+
+        public static bool SimpleDigit(int number)
+        {
+            if (number <= 0)
+                throw new Exception("Число не может быть меньше единицы!");
+            int count = 0;
+            for (int i = 1; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                    if (count > 1)
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using ConsoleApp1;
+
+namespace TestProject
+{
+    public class Tests
+    {
+        [TestCase(new int[] { 1, 4, 7, 3, 6, 8, 2, 0, 9, 5 })]
+        [TestCase(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
+        public void TestSorting(int[] testString)
+        {
+            int[] expected = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Assert.IsTrue(Enumerable.SequenceEqual(Lab1.Sorting(testString), expected));
+        }
+        [TestCase(new int[] { })]
+        public void TestSortingException(int[] testString)
+        {
+            var errorMessage = Assert.Throws<Exception>(() => Lab1.Sorting(testString));
+            StringAssert.Contains("Массив не может быть пустым! ", errorMessage.ToString());
+        }
+        [TestCase("123454321")]
+        [TestCase("!:%?((?%:!")]
+        public void TestPalindrom(string testString)
+        {
+            Assert.IsTrue(Lab1.Palindrom(testString));
+        }
+        [TestCase("1231")]
+        [TestCase("!3Daô!")]
+        public void TestPalindromFalse(string testString)
+        {
+            Assert.IsFalse(Lab1.Palindrom(testString));
+        }
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void TestFactorialException(int testNumber)
+        {
+            var errorMessage = Assert.Throws<Exception>(() => Lab1.Factorial(testNumber));
+            StringAssert.Contains("Число не может быть меньше единицы! ", errorMessage.ToString());
+        }
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(3, ExpectedResult = 6)]
+        [TestCase(5, ExpectedResult = 120)]
+        public int TestFactorial(int testNumber)
+        {
+            return Lab1.Factorial(testNumber);
+        }
+        [TestCase(0, ExpectedResult = 0)]
+        [TestCase(2, ExpectedResult = 1)]
+        [TestCase(6, ExpectedResult = 8)]
+        public int TestFibonacci(int testNum)
+        {
+            return Lab1.Fibonacci(testNum);
+        }
+        [TestCase(-1)]
+        [TestCase(-100)]
+        public void TestFibonacciException(int testNum)
+        {
+            var errorMessage = Assert.Throws<Exception>(() => Lab1.Fibonacci(testNum));
+            StringAssert.Contains("Число не может быть меньше нуля!", errorMessage.ToString());
+        }
+        [TestCase(-12, 0, ExpectedResult = 1)]
+        [TestCase(14, 1.7, ExpectedResult = 88.801)]
+        [TestCase(12.643, -2.4, ExpectedResult = 0.002)]
+        [TestCase(7.96, 4.31, ExpectedResult = 7637.22)]
+        public double TestDoubleNum(double testNumber, double testDegree)
+        {
+            return Lab1.DoubleNum(testNumber, testDegree);
+        }
+        [TestCase(11)]
+        [TestCase(23)]
+        public void TestSimpleDigit(int testNumber)
+        {
+            Assert.IsTrue(Lab1.SimpleDigit(testNumber));
+        }
+        [TestCase(0)]
+        [TestCase(-7)]
+        public void TestSimpleDigitException(int testNumber)
+        {
+            var errMes = Assert.Throws<Exception>(() => Lab1.SimpleDigit(testNumber));
+            StringAssert.Contains("Число не может быть меньше единицы!", errMes.ToString());
+        }
+    }
+}
